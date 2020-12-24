f960dacafe43875b51059adf606c9809062612a4
saif-mhr <saifullah786@hotmail.com>
2020-12-25 00:53:32 +0500

    Add project files.

---
+++ TempData/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempData.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string[] str = { "saifullah", "mahar", "Faqeer" };
+            TempData["var1"] =str;
+            
+
+            TempData["var2"] = new List<string>()
+            {
+                "saif",
+                 "mahar",
+                  "faqeer",
+            };
+            return View();
+        }
+
+        public IActionResult about()
+        {
+            TempData.Keep();
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
